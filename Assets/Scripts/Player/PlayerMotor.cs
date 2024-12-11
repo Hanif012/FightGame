a4f9927c34ee217f9294125dc4a2601124199f4e
snlasd <165441103+snlasd@users.noreply.github.com>
2024-12-11 19:12:22 +0700

    Player Movement - Lx

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -20,7 +20,25 @@ public class PlayerMotor : MonoBehaviour
     public void ProcessMove(Vector2 input)
     {
         Vector3 moveDirection = Vector3.zero;
-        moveDirection.z = -input.x;
+        moveDirection.z = input.x;
+        // mengecek LX
+        if (0 < moveDirection.z && moveDirection.z <= 0.5)
+        {
+            // 0 < Lx <= 0.5 Walk
+            Debug.Log("Walk");
+            speed = 2;
+        }
+        else if (0.5 < moveDirection.z && moveDirection.z < 1)
+        {
+            // 0.5 < Lx < 1 Run
+            Debug.Log("Run");
+            speed = 5;
+        }
+        else if (moveDirection.z == 0) 
+        {
+            Debug.Log("Idle");
+        }
+
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
     }
 }
